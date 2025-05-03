acc8122af3811744c4f87b5daaf3ab0e1b81a300
Alex Clifton <alex@clifton.org.uk>
2025-05-03 01:16:14 +0100

    Removed typing effect

--- NextJS/ajclifton/src/app/page.tsx
+++ NextJS/ajclifton/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className="bg-zinc-100 dark:bg-zinc-800 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-grey-500 dark:text-white text-3xl font-bold tracking-wider text-center content-stretch"><TypeEffect text="> AJClifton" delay={300} startingIndex={2} initialDelay={1000}/><Cursor blinks={10} blinkTime={530} /></h1>
+        <h1 className="text-grey-500 dark:text-white text-3xl font-bold tracking-wider text-center content-stretch">AJClifton</h1>
         <p className="text-grey-500 dark:text-white text-base">I am working on this website very slowly.</p>
       </main>
 
